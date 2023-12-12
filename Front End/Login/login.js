51e46e08f1c217e2fa83e3168227bb421826b988
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-12-12 19:43:40 +0530

    Creating Login Screen Frontend

---
+++ Front End/Login/login.js
@@ -0,0 +1,19 @@
+async function login(e){
+    try{
+        e.preventDefault();
+        console.log(e.target.email.value);
+
+        const loginDetails = {
+            email : e.target.email.value,
+            password : e.target.password.value
+        }
+
+        console.log(loginDetails);
+
+        // const response = await axios.post('http://localhost:3000/user/login',loginDetails)
+            
+    }catch(err){
+        console.log(JSON.stringify(err));
+        document.body.innerHTML += `<div style = "color:red;">${err}</div>`;
+    }
+}
