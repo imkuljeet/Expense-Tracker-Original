e2c7bc792211f84f002a4ac9805efb309d5289d2
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-12-22 06:12:05 +0530

    Not Showing Leaderboard total expense error solved
    Total expense was not showing before

--- Back End/controller/premiumFeature.js
+++ Back End/controller/premiumFeature.js
@@ -1,24 +1,22 @@
 const User = require('../models/users');
 const Expense = require('../models/expenses');
-const sequelize = require('../util/database');
-const e = require('express');
+const sequelize =require('../util/database');
+
+
 
 const getUserLeaderBoard = async (req, res) => {
     try{
         const leaderboardofusers = await User.findAll({
+            order: [["totalExpenses", 'DESC']]
             
-            order:[['totalExpenses', 'DESC']]
-
         })
-       
-        res.status(200).json(leaderboardofusers)
-    
+        // console.log(leaderboardofusers);
+        res.status(200).json(leaderboardofusers)     
 } catch (err){
     console.log(err)
     res.status(500).json(err)
 }
 }
-
 module.exports = {
     getUserLeaderBoard
 }
