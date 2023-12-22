a77a036333d739076db8d6fde4f2dec71d75736a
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-12-23 00:42:15 +0530

    Making Expense App Production ready
    Delete expense working with transaction

--- Back End/controller/expense.js
+++ Back End/controller/expense.js
@@ -49,20 +49,46 @@ const getexpenses = (req, res)=> {
     })
 }
 
-const deleteexpense = (req, res) => {
+const deleteexpense = async (req, res) => {
+    const t = await sequelize.transaction();
+    try{
+
     const expenseid = req.params.expenseid;
     if(expenseid == undefined || expenseid.length === 0){
         return res.status(400).json({success: false, })
     }
-    Expense.destroy({where: { id: expenseid, userId: req.user.id }}).then((noofrows) => {
-        if(noofrows === 0){
+
+    const expense = await Expense.findByPk(expenseid);
+    await User.update({
+        totalExpenses: req.user.totalExpenses - expense.expenseamount
+    },{where: {id: req.user.id}},{transaction: t});
+
+    // console.log("expense is here",expense);
+
+    // const totalExpense = Number(req.user.totalExpenses) - Number(expense.expenseamount);
+
+    // await User.update(
+    //     {
+    //         totalExpenses: totalExpense,
+    //     },
+    //     {
+    //         where: { id: req.user.id },
+    //         transaction: t,
+    //     }
+    // );
+
+
+    let destroy = await Expense.destroy({where: { id: expenseid, userId: req.user.id }},{transaction: t});
+        if(destroy === 0){
+            await t.rollback();
             return res.status(404).json({success: false, message: 'Expense doenst belong to the user'})
         }
         return res.status(200).json({ success: true, message: "Deleted Successfuly"})
-    }).catch(err => {
+    }catch(err) {
         console.log(err);
+        await t.rollback();
         return res.status(500).json({ success: true, message: "Failed"})
-    })
+    }
 }
 
 module.exports = {
